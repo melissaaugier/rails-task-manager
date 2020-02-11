2a9f1d99a982970d9b7064f6b67fb4bf84fb6314
Melissa Augier <augiermelissa@gmail.com>
2020-02-11 15:21:35 +0100

    exercise tasks done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # lister toutes les tasks
+  get 'tasks', to: 'tasks#index'
+
+  # lister 1 task
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # creer une task
+  get 'new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # editer une task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # supprimer une task
+  delete 'tasks/:id', to: 'tasks#destroy'
+
+  root 'tasks#index'
 end
